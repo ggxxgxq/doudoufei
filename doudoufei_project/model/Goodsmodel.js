d3345dc2f7132c37d3f05bd5511e13e685977901
ggxxgxq <1356857447@qq.com>
2018-02-28 23:27:47 +0800

    商品添加功能

---
+++ doudoufei_project/model/Goodsmodel.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// 创建文档的定义
+var Goods = new Schema({
+    goods_name  : String,
+    goods_id    : String,
+    goods_price : String,
+    goods_pic   : String,
+    create_date : { type: Date, default: Date.now }
+});
+
+// 创建model对象，与数据库中的文档（表）映射
+var Goodsmodel = mongoose.model('goods',Goods);
+module.exports = Goodsmodel; 
--- doudoufei_project/routes/index.js
+++ doudoufei_project/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var md5 = require("md5");
-var Usermodel = require("../model/Usermodel")
+var Usermodel = require("../model/Usermodel");
+var Goodsmodel = require("../model/Goodsmodel");
+var multiparty = require('multiparty');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,10 +28,40 @@ router.get('/mainright-new', function(req, res, next) {
   res.render('mainright-new');
 });
 router.get('/mainright-list', function(req, res, next) {
-  res.render('mainright-list');
+  Goodsmodel.find({},function(err,docs){
+    res.render('mainright-list',{list:docs});
+  })
 });
 router.get('/manage', function(req, res, next) {
   res.render('manage');
+});
+router.post('/api/add_goods', function(req, res) {
+    var form = new multiparty.Form({
+      uploadDir :"./public/images"
+    });
+ 
+      form.parse(req, function(err, body, files) {
+          //转换后的对象都是数组形式
+          var goods_name = body.goods_name[0];
+          var goods_id = body.goods_id[0];
+          var goods_price = body.goods_price[0];
+          var imgName = files.goods_pic[0].path;
+          var imgName = imgName.substr(imgName.lastIndexOf("\\")+1)
+          console.log(goods_name,goods_id,goods_price,imgName)
+
+          var gm = new Goodsmodel();
+          gm.goods_name= goods_name;
+          gm.goods_id= goods_id;
+          gm.goods_price= goods_price;
+          gm.img= imgName;
+          gm.save(function(err){
+              if( !err ){
+                res.send("文件上传成功");
+              }else{
+                res.send("文件上传失败")
+              }
+          })
+    });
 });
 router.post('/api/login',function(req,res){
     var username = req.body.username;
